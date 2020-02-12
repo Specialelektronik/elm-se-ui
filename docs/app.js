9ff168d670604096da0cdb73ad27e3cf6130a7fd
Carl-Fredrik Herö <carlfredrik.hero@gmail.com>
2020-02-12 23:50:31 +0100

    Add elm live and ui explorer

--- docs/app.js
+++
@@ -1,9 +0,0 @@
-import { Elm } from "../src/UI.elm";
-
-var app = Elm.UI.init({
-  node: document.getElementById("elm-f0111bc4e658d0f98db96260c16f7e49")
-});
-if (document.getElementById("elm-f0111bc4e658d0f98db96260c16f7e49")) {
-  document.getElementById("elm-f0111bc4e658d0f98db96260c16f7e49").innerText =
-    "This is a headless program, meaning there is nothing to show here.\n\nI started the program anyway though, and you can access it as `app` in the developer console.";
-}
